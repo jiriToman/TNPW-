0d6b487789ead830187bcc3fa4279cf5107389ff
JiriToman <toman.jiri@outlook.com>
2020-09-08 01:03:53 +0200

    pridani empty hbs

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const hbs = require('hbs');
-app.use(express.static('public'))//Sets a basic route
+app.use(express.static('resources'))//Sets a basic route
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
 app.get('/', (req, res) => res.send('Hello World !'));
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
